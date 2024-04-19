6c40e5689d3e3e10bc3fd30607e5d82942655e80
krig6 <ej.ldsm@gmail.com>
2024-04-19 14:34:38 +0800

    Add return values in playRound()
    Added outcome as a return value to provide accuracy in our score counter logic

--- script.js
+++ script.js
@@ -15,10 +15,16 @@ function playRound(playerSelection, computerSelection) {
     // Create conditional statement to determine if round is a win, defeat or draw
     if (playerSelection === computerSelection) {
         console.log(`Draw!`);
+        let outcome = 'draw';
+        return outcome;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection === 'scissors') {
         console.log(`Win!`);
+        let outcome = 'win';
+        return outcome;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log(`Lose`);
+        console.log(`Lose!`);
+        let outcome = 'lose'
+        return outcome;
     }
 }
 // playRound(playerSelection, computerSelection) // Test: Check if can play a round and must print a message if win, defeat or draw : DONE!
