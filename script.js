c5b673d53838ae0848b2a41479c06fe728b81f51
krig6 <ej.ldsm@gmail.com>
2024-04-19 12:04:01 +0800

    Create function playRound()
    Function to let users play a round with the computer
    
    With parameters, playerSelection and computerSelection to enable comparison and declare a winner

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ const getComputerChoice = () => {
 }
 // Test: Check if output is correct and giving random values : DONE!
 
-// Create a function to play a round with parameters: playerSelection and computerSelection
-
+let computerSelection = getComputerChoice(); // So we can pass the random value to playRound();
 // Prompt user to choose between 'rock', 'paper', 'scissors'
+let playerSelection = prompt("rock, paper, scissors");
+// Create a function to play a round with parameters: playerSelection and computerSelection
+function playRound(playerSelection, computerSelection) {
+    // Create conditional statement to determine if round is a win, defeat or draw
+    if (playerSelection === computerSelection) {
+        console.log(`Draw!`);
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log(`Win!`);
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log(`Lose`);
+    }
+}
 
-// Create conditional statement to determine if round is a win, defeat or draw
+playRound(playerSelection, computerSelection) // Test: Check if can play a round and must print a message if win, defeat or draw : DONE!
 
-// Test: Check if can play a round and must print a message if win, defeat or draw :
