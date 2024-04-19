5b6f4f5f926d6048d6bb2b0d2b5160efa9741c32
krig6 <ej.ldsm@gmail.com>
2024-04-19 11:38:40 +0800

    Create function getComputerChoice()
    Function in the game to make the computer's play

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Create a function to get random choice from the computer
-
-// Create an array to hold 'rock', 'paper', 'scissors'
-
-// Return random value from the array 
-
-// Testing: Check if output is correct and giving random values
+const getComputerChoice = () => {
+    // Create an array to hold 'rock', 'paper', 'scissors'
+    const getRandomChoice = ['rock', 'paper', 'scissors'];
+    // Return random value from the array 
+    return getRandomChoice[Math.floor(Math.random() * getRandomChoice.length)];
+}
+// Test: Check if output is correct and giving random values : DONE!
 
 // Create a function to play a round with parameters: playerSelection and computerSelection
 
@@ -12,4 +13,4 @@
 
 // Create conditional statement to determine if round is a win, defeat or draw
 
-// Testing: Check if can play a round and must print a message if win, defeat or draw
+// Test: Check if can play a round and must print a message if win, defeat or draw :
