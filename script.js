c979467ab6cbaf8d8265139f3521949514f30b58
krig6 <ej.ldsm@gmail.com>
2024-04-19 11:08:39 +0800

    Create pseudocode for getComputerChoice and playRound

---
+++ script.js
@@ -0,0 +1,15 @@
+// Create a function to get random choice from the computer
+
+// Create an array to hold 'rock', 'paper', 'scissors'
+
+// Return random value from the array 
+
+// Testing: Check if output is correct and giving random values
+
+// Create a function to play a round with parameters: playerSelection and computerSelection
+
+// Prompt user to choose between 'rock', 'paper', 'scissors'
+
+// Create conditional statement to determine if round is a win, defeat or draw
+
+// Testing: Check if can play a round and must print a message if win, defeat or draw
