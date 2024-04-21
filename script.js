baa2938a42fd3fa2ef8ec13b5fdd2ae5e809b32a
krig6 <ej.ldsm@gmail.com>
2024-04-21 13:03:00 +0800

    chore(formatting): Format code using TypeScript and JavaScript Language Features
    Ensure consistent code style across the project.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const getComputerChoice = () => {
-    const getRandomChoice = ['rock', 'paper', 'scissors'];
+    const getRandomChoice = ["rock", "paper", "scissors"];
     return getRandomChoice[Math.floor(Math.random() * getRandomChoice.length)];
-}
+};
 
 function ask(question, yes, no) {
-    if (confirm(question)) yes()
+    if (confirm(question)) yes();
     else no();
 }
 function game() {
@@ -18,15 +18,27 @@ ask("Try your luck?", game, exit);
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log(`It's a draw! Both unsheathed "${playerSelection}" weapons!`);
-        let outcome = 'draw';
+        let outcome = "draw";
         return outcome;
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log(`You win! Mighty ${playerSelection} trumps ${computerSelection}!`);
-        let outcome = 'win';
+    } else if (
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "rock" && computerSelection === "scissors")
+    ) {
+        console.log(
+            `You win! Mighty ${playerSelection} trumps ${computerSelection}!`
+        );
+        let outcome = "win";
         return outcome;
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log(`You lose! Better than ${playerSelection}? ${computerSelection}!`);
-        let outcome = 'lose'
+    } else if (
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "scissors" && computerSelection === "rock")
+    ) {
+        console.log(
+            `You lose! Better than ${playerSelection}? ${computerSelection}!`
+        );
+        let outcome = "lose";
         return outcome;
     }
 }
@@ -43,7 +55,9 @@ function playGame() {
             console.log("Game cancelled. No match winner!");
             return;
         }
-        while (!["rock", "paper", "scissors"].includes(playerSelection.toLowerCase())) {
+        while (
+            !["rock", "paper", "scissors"].includes(playerSelection.toLowerCase())
+        ) {
             alert("Please choose between rock, paper or scissors!");
             playerSelection = prompt("Rock, paper, scissors. Shoot!");
             if (playerSelection !== null) {
@@ -59,19 +73,27 @@ function playGame() {
         console.log(`Player threw in ${playerSelection.toLowerCase()}!`);
         console.log(`Computer threw in ${computerSelection}!`);
         let result = playRound(playerSelection, computerSelection);
-        if (result === 'win') {
+        if (result === "win") {
             playerScore++;
-        } else if (result === 'lose') {
+        } else if (result === "lose") {
             computerScore++;
         }
-        console.log(`Player has ${playerScore} point${playerScore === 0 ? '' : (playerScore === 1 ? '' : 's')}.`);
-        console.log(`Computer has ${computerScore} point${computerScore === 0 ? '' : (computerScore === 1 ? '' : 's')}.`);
+        console.log(
+            `Player has ${playerScore} point${playerScore === 0 ? "" : playerScore === 1 ? "" : "s"}.`
+        );
+        console.log(
+            `Computer has ${computerScore} point${computerScore === 0 ? "" : computerScore === 1 ? "" : "s"}.`
+        );
     }
     if (playerScore > computerScore) {
-        alert(`You won with ${playerScore} point${playerScore === 0 ? '' : (playerScore === 1 ? '' : 's')}. Try buying a lottery ticket next time!`);
+        alert(
+            `You won with ${playerScore} point${playerScore === 0 ? "" : playerScore === 1 ? "" : "s"}. Try buying a lottery ticket next time!`
+        );
     } else if (computerScore > playerScore) {
-        alert(`You lost and computer won with ${computerScore} point${computerScore === 0 ? '' : (computerScore === 1 ? '' : 's')}!`);
-    } else if (playerScore = computerScore) {
+        alert(
+            `You lost and computer won with ${computerScore} point${computerScore === 0 ? "" : computerScore === 1 ? "" : "s"}!`
+        );
+    } else if ((playerScore = computerScore)) {
         alert(`Meh! A tie!`);
     }
 }
